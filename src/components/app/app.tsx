77ea5b2c6ae0a376648d6dcfe60465d96bbbdd7b
QWAR-svg <zaz765831@gmail.com>
2021-08-05 16:58:40 +0300

    TS

---
+++ src/components/app/app.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { ITodo } from "../../types/data";
+
+const App: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const addTodo = () => {
+    if (value) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: value,
+          complete: false,
+        },
+      ]);
+      setValue("");
+    }
+  };
+  return (
+    <div>
+      <div>
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          type="text"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
